1bff7033ecf75d724426a5c4901a02200f1c6ab7
Abi Avudaiappan <abiavudaiappan2922-prog@gmail.com>
2026-07-16 14:21:27 +0530

    Fixed WhatsApp button

--- components/WhatsApp.tsx
+++ components/WhatsApp.tsx
@@ -1,14 +1,10 @@
 export default function Whatsapp() {
-  const message = encodeURIComponent(
-    "Hello, I would like to know more about Akash Tuition Center."
-  );
-
   return (
     <a
-      href={`https://api.whatsapp.com/send?phone=919087580456&text=${message}`}
+      href="https://api.whatsapp.com/send?phone=919087580456"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-6 right-6 bg-green-500 text-white px-5 py-3 rounded-full shadow-2xl font-bold"
+      className="fixed bottom-6 right-6 bg-green-500 text-white px-5 py-3 rounded-full shadow-2xl font-bold z-50"
     >
       WhatsApp
     </a>
